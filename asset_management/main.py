701a76c3453a264ebd82526d1ae65340f9f92871
swfs0417 <apdl0012@gmail.com>
2026-01-06 16:18:39 +0900

    Add CORS setting

--- asset_management/main.py
+++ asset_management/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from asset_management.app.user.routes import router as user_router
 
 app = FastAPI(title="Asset Management API")
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://d1vqqxs5v1ouxk.cloudfront.net",
+                   "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
